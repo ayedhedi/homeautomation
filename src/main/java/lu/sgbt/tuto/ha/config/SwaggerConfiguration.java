47094beb2c8fd4b8c08e0055bfb13e86a1e261a1
Hedi Ayed <ayed.hedi@gmail.com>
2017-11-15 22:47:54 +0100

    add swagger support

---
+++ src/main/java/lu/sgbt/tuto/ha/config/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package lu.sgbt.tuto.ha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Hedi Ayed on 15/11/2017.
+ *
+ *
+ * --------------------
+ *
+ * @author Hedi Ayed
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("lu.sgbt.tuto.ha.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
